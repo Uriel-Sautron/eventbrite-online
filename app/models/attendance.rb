92e4e8ce0eb831f3a7ff69fb1aa4ef4ccac0a5e4
Joe-Pumpkin <jonathan.lalanne@outlook.fr>
2020-11-02 18:50:47 +0100

    Monday offline

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,10 @@
 class Attendance < ApplicationRecord
   belongs_to :user
   belongs_to :event
+
+  after_create :event_send
+
+  def event_send
+      AttendanceMailer.join_event(self).deliver_now
+    end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,10 +2,14 @@ class Event < ApplicationRecord
   has_many :attendances
   has_many :users, through: :attendances
   belongs_to :user
-  validates :start_date, presence: true, if: :start_date > Time.now
-  validates :duration, presence: true numericality: {greater_than: 0 } if: :duration%5 == 0
-  validates :title, presence: true, lenght { in: 5..140 }
-  validates :description, presence: true, lenght { in: 20..1000 }
-  validates :price, presence: true numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 1000}
+
+  validates :star_date, presence: true#, if: :star_date > Time.now
+  validates :duration, presence: true# numericality: {greater_than: 0 } if: :duration%5 == 0
+  validates :title, presence: true#, lenght { in: 5..140 }
+  validates :description, presence: true#, lenght { in: 20..1000 }
+  validates :price, presence: true# numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 1000}
   validates :location, presence: true
+
+ 
+
 end
